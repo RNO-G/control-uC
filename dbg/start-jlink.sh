4791d40a72573d2336537c6ac9b8a17d4397c463
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2022-06-02 16:57:09 -0500

    simplify device selection

--- dbg/start-jlink.sh
+++ dbg/start-jlink.sh
@@ -1,2 +1,12 @@
 #/bin/sh 
-DISPLAY=:notarealdisplay sudo JLinkGDBServer -if SWD -device ATSAMD21J18 $@
+
+SN=""
+
+if [[ -z "${JLINK_SERIAL}" ]] 
+then 
+  echo "No serial number specified, define JLINK_SERIAL if you want to" 
+else
+  SN="-select usb=${JLINK_SERIAL}"
+fi 
+
+DISPLAY=:notarealdisplay sudo JLinkGDBServer -if SWD -device ATSAMD21J18 $SN $@
