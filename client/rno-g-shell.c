3978190de0ac87392b5a285321f9aee84abd4e87
Naren Kasinath <kasikav@outlook.com>
2022-07-13 10:08:51 -0500

    Use a boolean to run the loop to avoid redundancy

--- client/rno-g-shell.c
+++ client/rno-g-shell.c
@@ -3,16 +3,15 @@
 int main() {
     size_t buf_size = 0; // initial value is 0 so getline will adjust it
     size_t len;
+    bool run = true;
     char * buf = NULL; // initial value is NULL so getline will malloc
     
-    while(true) {
+    while(run) {
         printf("rno-g-shell > ");
         len = getline(&buf, &buf_size, stdin);
         
-        if (!strcmp(buf, "quit\n")) {
-            free(buf);
-            buf = NULL;
-            buf_size = 0;
+        if (!strcmp(buf, "QUIT\n")) {
+            run = false;
             break;
         }
         else {
