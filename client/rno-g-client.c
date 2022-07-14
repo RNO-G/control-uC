f134ba9b24e9a7124a7bb93291d56f5ed7fb81a2
Naren Kasinath <kasikav@outlook.com>
2022-07-13 23:42:35 -0500

    Move rno-g-shell.c to rno-g-client.c

---
+++ client/rno-g-client.c
@@ -0,0 +1,26 @@
+#include "rno-g-client.h"
+
+int main() {
+    size_t buf_size = 0; // initial value is 0 so getline will adjust it
+    size_t len;
+    bool run = true;
+    char * buf = NULL; // initial value is NULL so getline will malloc
+    
+    while(run) {
+        printf("rno-g-shell > ");
+        len = getline(&buf, &buf_size, stdin);
+        
+        if (!strcmp(buf, "QUIT\n")) {
+            run = false;
+        }
+        else {
+            parse_cmd(buf);
+        }
+
+        free(buf);
+        buf = NULL;
+        buf_size = 0;
+    }
+
+    return 0;
+}
