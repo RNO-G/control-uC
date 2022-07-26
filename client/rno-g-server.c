62f4c096a783f70a7fa4b94030ab2aac575f3510
Naren Kasinath <kasikav@outlook.com>
2022-07-26 11:43:22 -0500

    Use a fixed-size array instead of a linked list for the command queue

--- client/rno-g-server.c
+++ client/rno-g-server.c
@@ -1,116 +1,93 @@
 #include "rno-g-server.h"
 
-struct cmd_node {
-    char * cmd;
-    struct cmd_node * next;
-    struct cmd_node * prev;
-};
-
 int server_socket;
-int queue_running = 1;
-size_t num_clients = 0;
-cmd_node * head = NULL;
-cmd_node * tail = NULL;
+
+char cmd_queue[CMD_LIM][BUF_SIZE];
+int cmd_queue_running = 1;
+size_t cmd_queue_len = 0;
+pthread_t cmd_queue_manager;
+pthread_mutex_t cmd_queue_mutex;
+
 
 void signal_handler(int sig) {
     if (sig == SIGINT) {
+        cmd_queue_running = 0;
+
         error_check(close(server_socket));
+        error_check(pthread_join(cmd_queue_manager, NULL));
+        error_check(pthread_mutex_destroy(&cmd_queue_mutex));
         exit(EXIT_SUCCESS);
     }
 }
 
-int cmd_enqueue(char * cmd_str) {
-    cmd_node * node = (cmd_node *) malloc(sizeof(cmd_node));
-
-    node->cmd = (char *) malloc(sizeof(char) * BUF_SIZE);
-    strcpy(node->cmd, cmd_str);
-    
-    if (head == NULL && tail == NULL) {
-        head = node;
-        node->next = NULL;
-        node->prev = NULL;
+int cmd_enqueue(char * cmd) {
+    if (cmd_queue_len == CMD_LIM) {
+        return -1;
     }
     else {
-        tail->next = node;
-        node->next = NULL;
-        node->prev = tail;
+        strcpy(cmd_queue[cmd_queue_len], cmd);
+        cmd_queue_len++;
     }
-
-    tail = node;
 
     return 0;
 }
 
 int cmd_dequeue() {
-    if (head == NULL && tail == NULL) {
+    if (cmd_queue_len == 0) {
         return -1;
     }
-
-    cmd_node * tmp = head->next;
-
-    free(head->cmd);
-    free(head);
-
-    if (tmp == NULL) {
-        head = NULL;
-        tail = NULL;        
-    }
     else {
-        head = tmp;
+        for (int i = 1; i < CMD_LIM; i++) {
+            strcpy(cmd_queue[i - 1], cmd_queue[i]);
+        }
+
+        cmd_queue_len--;
     }
 
     return 0;
 }
 
 int cmd_flush() {
-    if (head == NULL && tail == NULL) {
-        return -1;
-    }
-
-    cmd_node * cur = head;
+    memset(cmd_queue, 0, sizeof(char) * CMD_LIM * BUF_SIZE);
     
-    while(cur) {
-        cmd_dequeue();
-        cur = head;
-    }
-
     return 0;
 }
 
-void print_cmd(char * cmd, char * client_address) {
-    fputs("RECIEVED {", stdout);
-    fputs(cmd, stdout);
-    fputs("} FROM CLIENT AT {", stdout);
-    fputs(client_address, stdout);
-    fputs("}\n", stdout);
+void print_cmd_queue() {
+    fputs("COMMAND QUEUE : ", stdout);
+    
+    for (int i = 0; i < cmd_queue_len; i++) {
+        fputs("{", stdout);
+        fputs(cmd_queue[i], stdout);
+        fputs("} ", stdout);
+    }
+
+    fputs("\n", stdout);
 }
 
-void * manage_queue() {
-    while (queue_running) {
-        cmd_node * cur = head;
+void * manage_cmd_queue() {
+    char * cmd;
+    
+    while (cmd_queue_running) {
+        pthread_mutex_lock(&cmd_queue_mutex);
 
-        if (cur) {
-            fputs("Scheduler is Processing {", stdout);
-            fputs(cur->cmd, stdout);
-            fputs("}\n", stdout);
+        print_cmd_queue();
 
-            if (!strcmp(cur->cmd, "LTE-OFF")) {
-                fputs("Flushing Queue\n", stdout);
+        if (cmd_queue_len > 0) {
+            cmd = cmd_queue[0];
+            cmd_dequeue();
+
+            if (!strcmp(cmd, "LTE-OFF")) {
+                fputs("FLUSHING COMMAND QUEUE\n", stdout);
                 cmd_flush();
 
-                queue_running = 0;
-                num_clients = 0;
+                cmd_queue_running = 0;
             }
-            else {
-                if (!strcmp(cur->cmd, "DISCONNECT")) {
-                    num_clients--;
-                }
-
-                cmd_dequeue();
-            }  
         }
 
-        sleep(1);
+        pthread_mutex_unlock(&cmd_queue_mutex);
+
+        sleep(5);
     }
 
     pthread_exit(EXIT_SUCCESS);
@@ -118,7 +95,6 @@ void * manage_queue() {
 
 void * manage_client(void * client_socket_ptr) {
     int client_socket = *((int *) client_socket_ptr);
-    // free(client_socket_ptr);
 
     char cmd[BUF_SIZE];
     char ack[BUF_SIZE] = "Command Recieved\n";
@@ -126,14 +102,18 @@ void * manage_client(void * client_socket_ptr) {
     while (1) {
         error_check(read(client_socket, cmd, BUF_SIZE));
 
-        if (queue_running) {
-            cmd_enqueue(cmd);
+        if (cmd_queue_running) {
+            pthread_mutex_lock(&cmd_queue_mutex);
+            
+            if (cmd_enqueue(cmd) == -1) {
+                fputs("SERVER COMMAND QUEUE FULL, TRY AGAIN LATER\n", stderr);
+            }
+            
+            pthread_mutex_unlock(&cmd_queue_mutex);
         }
         else {
             fputs("SERVER ASLEEP\n", stdout);
         }
-
-        // print_cmd(cmd, client_address);
 
         error_check(write(client_socket, ack, BUF_SIZE));
 
@@ -148,15 +128,13 @@ void * manage_client(void * client_socket_ptr) {
 
 int main(int argc, char ** argv) {
     int client_socket;
-    
-    int * client_socket_ptr;
-    
-    pthread_t thread;
+        
+    pthread_t client;
     pthread_attr_t attr;
     
     struct sockaddr_in server_addr;
-    
-    cmd_node * cur;
+
+    memset(cmd_queue, 0, sizeof(char) * CMD_LIM * BUF_SIZE);    
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_port = htons(PORT);
@@ -164,30 +142,26 @@ int main(int argc, char ** argv) {
 
     error_check(server_socket = socket(AF_INET, SOCK_STREAM, 0));
     error_check(bind(server_socket, (struct sockaddr *) &server_addr, sizeof(server_addr)));
-    error_check(listen(server_socket, CLIENT_LIM));
+    error_check(listen(server_socket, CLI_LIM));
+    error_check(pthread_mutex_init(&cmd_queue_mutex, NULL));
+    error_check(pthread_create(&cmd_queue_manager, NULL, manage_cmd_queue, NULL));
 
     signal(SIGINT, signal_handler);
-
-    pthread_attr_init(&attr);
-    pthread_attr_setdetachstate(&attr, 1);
-    pthread_create(&thread, &attr, manage_queue, NULL);
-    pthread_attr_destroy(&attr);
 
     while (1) {
         error_check(client_socket = accept(server_socket, (struct sockaddr *) NULL, NULL));
         
-        // client_socket_ptr = (int *) malloc(sizeof(int));
-        // *client_socket_ptr = client_socket;
-        
         pthread_attr_init(&attr);
         pthread_attr_setdetachstate(&attr, 1);
-        pthread_create(&thread, &attr, manage_client, &client_socket);
+        pthread_create(&client, &attr, manage_client, &client_socket);
         pthread_attr_destroy(&attr);
-
-        num_clients++;
     }
+
+    cmd_queue_running = 0;
     
     error_check(close(server_socket));
+    error_check(pthread_join(cmd_queue_manager, NULL));
+    error_check(pthread_mutex_destroy(&cmd_queue_mutex));
 
     return EXIT_SUCCESS;
 }
