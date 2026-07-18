0517d33a6920bb2636732a7dd78542fe97abf53f
Cosmin Deaconu <cozzyd@kicp.uchicago.edu>
2026-07-17 23:38:13 -0500

    need quotes I guess

--- include/rno-g-control.h
+++ include/rno-g-control.h
@@ -477,7 +477,7 @@ typedef struct rno_g_report_v4
                               "\"currents\": {\"sbc\": %d, \"surf\": [%d,%d,%d], \"dh\": [%d,%d,%d], \"lte\": %0.3f, \"lt\":%0.3f, \"radiant\": %0.3f, \"batt\": %0.3f, \"pv\": %0.3f, \"turb\": %0.3f }, "\
                               "\"voltages\": {\"lt\": %d, \"radiant\": %d, \"5v\": %u, \"3.3v\": %u, \"lte_a\": %u,  \"lte_d\": %u, \"batt\": %u, \"pv\": %d, \"turb\": %u }, "\
                               "\"temps\": {\"local\": %0.3f, \"remote_1\": %0.3f, \"remote_2\": %0.3f, \"micro\": %0.3f }, "\
-                              "\"when_analog\": %d, \"when_digi\": %d, \"when_power\": %d, \"when_temp\": %d, \"heater:\": %d, amps_fault: %u, sbc_pgood: %u,sbc_fault: %u"\
+                              "\"when_analog\": %d, \"when_digi\": %d, \"when_power\": %d, \"when_temp\": %d, \"heater:\": %d, \"amps_fault\": %u, \"sbc_pgood\": %u,\"sbc_fault\": %u"\
                               "\"power_state\":" RNO_G_POWER_STATE_JSON_FMT_V4 "}"
 
 
